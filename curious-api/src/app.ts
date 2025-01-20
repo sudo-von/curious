7c792bc93dd5461dae20923a53a34bf037674309
VoN <sudo.von.contact@gmail.com>
2025-01-19 18:27:29 -0600

    feat: :sparkles: add environment variables

--- curious-api/src/app.ts
+++ curious-api/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import './config/environment-variables'
 
 const app = express();
 const port = 3000;
---
+++ curious-api/src/config/environment-variables.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+export const ENVIRONMENT_VARIABLES = {
+  environment: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || '3000',
+};
