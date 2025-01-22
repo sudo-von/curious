b8e10da5b1378d2c9a8815ce51dc1cc5925b5abd
VoN <sudo.von.contact@gmail.com>
2025-01-22 01:03:17 -0600

    feat: :sparkles: add graphql support

---
+++ curious-api/src/configurations/graphql.configuration.ts
@@ -0,0 +1,20 @@
+import path from 'path';
+import { ApolloServer } from '@apollo/server';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+import { getUserByUsername, getUserById } from '../libraries/graphql/resolvers/queries/user.query';
+
+const typePath = path.join(__dirname, '..', 'libraries', 'graphql', 'schemas');
+const typeSources = loadFilesSync(typePath, { recursive: true });
+
+const typeDefs = mergeTypeDefs(typeSources);
+
+export const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      getUserByUsername,
+      getUserById,
+    },
+  },
+});
